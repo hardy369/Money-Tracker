20a41f73006e9f73e2d48b7220da4efa35b0a2b8
Hardhika Ramineni <134047944+hardy369@users.noreply.github.com>
2025-01-20 11:28:29 +0530

    Update Transaction.js

--- api/models/Transaction.js
+++ api/models/Transaction.js
@@ -1,13 +1,23 @@
-const mongoose = require('mongoose')
-const { model, Schema } = mongoose
+const mongoose = require('mongoose');
 
-const TransactionSchema = new Schema({
-    name: {type: String, required: true},
-    price: {type: Number, required: true},
-    description: {type: String, required: true},
-    datetime: {type: Date, required: true},
-})
+const TransactionSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String
+    },
+    datetime: {
+        type: Date,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    }
+}, {
+    timestamps: true
+});
 
-const TransactionModel = new model('transaction', TransactionSchema)
-
-module.exports = TransactionModel
+module.exports = mongoose.model('Transaction', TransactionSchema);
